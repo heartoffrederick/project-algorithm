2927291ff9e6e4584930bc819a593967b8e5e986
joshua <923500094@qq.com>
2019-08-08 21:09:49 +0800

    创建工程2

---
+++ src/main/java/com/heartoffrederick/pojo/User.java
@@ -0,0 +1,11 @@
+package com.heartoffrederick.pojo;
+
+/**
+ * @author Joshua·Wang
+ * @title: User
+ * @projectName project-algorithm
+ * @description: TODO
+ * @date 2019/8/8 21:09
+ */
+public class User {
+}
---
+++ src/main/java/com/heartoffrederick/util/JsonUtils.java
@@ -0,0 +1,11 @@
+package com.heartoffrederick.util;
+
+/**
+ * @author Joshua·Wang
+ * @title: JsonUtils
+ * @projectName project-algorithm
+ * @description: TODO
+ * @date 2019/8/8 21:09
+ */
+public class JsonUtils {
+}
